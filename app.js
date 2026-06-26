c53bf9507a320f9e6f1b9c20e7ac0091ae1d2d5e
rianprakasa <rianprakasa424@gmail.com>
2026-06-26 17:38:41 +0700

    perbaikan

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const diagnosisRoutes = require('./routes/diagnosisRoute.js');
 
 // Middleware untuk mem-parsing JSON
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Gunakan routing
 app.use('/api', diagnosisRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
